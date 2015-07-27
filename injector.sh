073385bec14408c8da3635dc2adac28a087818ae
Bart Reardon <bart.reardon@csiro.au>
2015-07-27 22:36:09 +1000

    Create injector.sh

---
+++ injector.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# injector
+
+# inject boot scripts into unbooted OS X system for thin clinet deployment
+# run from bootted USB drive/recovery partition
+
+# get boot volume (only works when not booted to the current boot volume)
+bootVol=$(df | grep $(bless -info --getboot) | awk '{printf $9; i = 10; while ($i != "") { printf " "$i; i++ }}')
+
+mkdir "$bootVol/etc/injector"
+
+# copy assets to boot volume
+
+cp /Install/addmacadmin.sh "$bootVol/etc/injector/"
+chmod a+x "$bootVol/etc/injector/addmacadmin.sh"
+
+cp /Install/addmacadmin.plist "$bootVol/Library/LaunchDaemons/addmacadmin.plist"
+
+cp -R /Install "$bootVol/etc/injector/"
+
+sleep 0.5
+
+touch "$bootVol/var/db/.AppleSetupDone"
+
+shutdown -r now
+
+# end injector
